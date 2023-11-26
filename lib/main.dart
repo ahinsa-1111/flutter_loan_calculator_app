c03ea77791357ed611cfae13825c1b81cd910f37
ahinsa-1111 <148263893+ahinsa-1111@users.noreply.github.com>
2023-11-26 10:00:35 +0530

    commit_03

--- lib/main.dart
+++ lib/main.dart
@@ -17,23 +17,104 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         leading: Icon(
           Icons.notes,
-          size: 20,
-          color: Colors.white,
+          color: Colors.black,
         ),
-        toolbarHeight: 25,
-        backgroundColor: Colors.blue,
+        toolbarHeight: 20,
+        backgroundColor: Colors.yellow,
         elevation: 0,
         actions: [
           Padding(
             padding: const EdgeInsets.only(right: 15.0),
-            child: Icon(
-              Icons.info,
-              size: 20,
-              color: Colors.white,
+            child: Icon(Icons.info,
+                size: 20, //left side icon
+                color: Colors.black),
+          ),
+        ],
+      ),
+      body: body(),
+    );
+  }
+
+  Widget body() {
+    return Container(
+      color: Colors.grey[100],
+      child: Column(
+        children: [
+          Container(
+            height: 80, //top yellow border
+            decoration: BoxDecoration(
+              color: Colors.yellow,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  bottom: 20.0), // car loan title alinment
+              child: Align(
+                alignment: Alignment.center,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Car Loan Calculator",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10, //first sized box alignment
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30, 10, 40, 0),
+            child: Column(
+              children: [
+                inputForm(title: "Car Price", hintText: "e.g 900000"),
+                inputForm(title: "Down Payment", hintText: "e.g 9000"),
+                inputForm(title: "Intrest Rate", hintText: "e.g 3.5"),
+                Text("Loan Period",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))
+              ],
             ),
           )
         ],
       ),
+    );
+  }
+
+  Widget inputForm({String? title, String? hintText}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title!,
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          height: 5, //input container alignment
+        ),
+        Container(
+          height: 40, //input container height
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(borderSide: BorderSide.none),
+                hintText: hintText),
+          ),
+        ),
+        SizedBox(
+          height: 10, //height between input container
+        )
+      ],
     );
   }
 }
